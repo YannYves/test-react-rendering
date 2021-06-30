f173fe7a29d549fa6cd18841c75e1cf64aa892a5
YannYves <yanndubois4@gmail.com>
2021-06-30 13:57:12 +0200

    test: add enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,17 @@
 import React from "react";
-import ReactShallowRenderer from "react-test-renderer/shallow";
+import ShallowRenderer from "react-test-renderer/shallow"; // ES6
 import Header from "../../components/Header";
+import { shallow } from "enzyme";
+
+// test("should render Header correctly", () => {
+//   const renderer = new ShallowRenderer();
+//   renderer.render(<Header />);
+//   expect(renderer.getRenderOutput()).toMatchSnapshot();
+// });
 
 test("should render Header correctly", () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
+  // expect(wrapper.find("h1").length).toBe(1);
+  // expect(wrapper.find("h1").text()).toBe("Expensify");
 });
